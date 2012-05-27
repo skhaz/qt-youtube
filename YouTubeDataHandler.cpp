36ae73a9e4a7878d284f4fce2bbf31e8433ff7ad
Rodrigo Delduca Batista <rodrigodelduca@gmail.com>
2012-05-27 20:52:50 -0300

    Partial support to youtube storyboard

--- YouTubeDataHandler.cpp
+++ YouTubeDataHandler.cpp
@@ -20,15 +20,30 @@ void YouTubeDataHandler::asyncStart(const QUrl& url)
 
 void YouTubeDataHandler::finished()
 {
-    int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    int statusCode = _reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
     if (statusCode >= 200 && statusCode < 300) {
-        // This block is based on file youtube.lua from VideoLAN project
-        QHash<int, QString> stream_map;
+        QByteArray data = reply->readAll();
         QRegExp re("\"url_encoded_fmt_stream_map\": \"([^\"]*)\"", Qt::CaseInsensitive, QRegExp::RegExp2);
         QRegExp urls("itag=(\\d+),url=(.*)");
+        QString storyboard("storyboard_spec=");
 
-        if (re.indexIn(reply->readAll()) != -1) {
+        if (int index = data.indexOf(storyboard)) {
+            index = index + storyboard.length();
+            QString temp = QUrl::fromPercentEncoding(data.mid(index, (data.indexOf("\\u0026", index)) - index));
+
+            qDebug() << temp;
+            QStringList temp2 = temp.split("|");
+            QString temp3 = temp2.at(2);
+
+            qDebug() << temp3.split("#");
+            // $L
+            // $N
+        }
+
+        if (re.indexIn(data) != -1) {
+            QHash<int, QString> stream_map;
             QString result = re.cap(1);
+
             foreach (QString line, result.split("\\u0026")) {
                 if (urls.indexIn(QUrl::fromPercentEncoding(line.toAscii())) != -1) {
                     stream_map[urls.cap(1).toInt()] = urls.cap(2);
@@ -37,10 +52,15 @@ void YouTubeDataHandler::finished()
 
             // XXX hardcoded
             QList<QString> values = stream_map.values();
-            qDebug() << stream_map;
-            emit completed(values.last());
+            emit completed(values.first());
         }
     }
+
+    else {
+        // TODO handle error
+    }
+
+    _reply->deleteLater();
 }
 
 void YouTubeDataHandler::error(QNetworkReply::NetworkError error)
